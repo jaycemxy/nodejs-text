e342723f0f16076878be6d0f15469d478e7e16b1
JayceMXY <jayce_ma.xa@foxmail.com>
2018-04-10 16:31:08 +0800

    第一版

--- main.js
+++ main.js
@@ -6,51 +6,45 @@ window.jQuery = function (nodeOrSelector) {
 }
 window.$ = window.jQuery
 
-window.Promise = function (fn) {
-    // ...
-    return {
-        then: function () {}
+window.jQuery.ajax = function ({url,method,body,successFn,failFn,headers}) {
+    let request = new XMLHttpRequest()  // 创建新对象
+    request.open(method, url)  // 配置这个request对象
+    for (let key in headers) {  
+        let value = headers[key]
+        request.setRequestHeader(key, value)
     }
-}
-
-window.jQuery.ajax = function ({url,method,body,headers}) {
-    return new Promise(function (resolve, reject) {
-        let request = new XMLHttpRequest()   //创建一个新对象request
-        request.open(method, url) // 初始化这个request对象
-        for (let key in headers) {
-            let value = headers[key]
-            request.setRequestHeader(key, value)
-        }
-        request.onreadystatechange = () => {   //监听这个对象的readystate变化
-            if (request.readyState === 4) {
-                if (request.status >= 200 && request.status < 300) {
-                    resolve.call(undefined, request.responseText)
-                } else if (request.status >= 400) {
-                    reject.call(undefined, request)
-                }
+    request.onreadystatechange = () => {  //监听这个对象的readystate变化
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                successFn.call(undefined, request.responseText)
+            } else if (request.status >= 400) {
+                failFn.call(undefined, request)
             }
         }
-        request.send(body)  //发送这个对象
-    })
+    }
+    request.send(body)  //发送这个对象
 }
 
+function f1(responseText) {}
+
+function f2(responseText) {}
+
 myButton.addEventListener('click', (e) => {
-    let promise = window.jQuery.ajax({
-        url: '/xxx',
+    window.jQuery.ajax({
+        url: '/frank',
         method: 'get',
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
             'frank': '18'
+        },
+        successFn: (x) => {
+            f1.call(undefined, x)
+            f2.call(undefined, x)
+        },
+        failFn: (x) => {
+            console.log(x)
+            console.log(x.status)
+            console.log(x.responseText)
         }
     })
-
-    promise.then(
-        (text) => {
-            console.log(text)
-        },
-        (request) => {
-            console.log(request)
-        }
-    )
-
 })
